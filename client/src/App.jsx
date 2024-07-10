0cc84afe949c0876f10f4131497895da5c2db7aa
NarutoxParth <114332684+NarutoxParth@users.noreply.github.com>
2024-07-10 15:43:46 +0530

    App.jsx Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signup from './pages/Signup'
+import Sigin from './pages/Sigin'
+import About from './pages/About'
+
 
 export default function App() {
-  return (
-    <div>App</div>
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/projects" element={<Projects />}/>
+        <Route path="/sign-up" element={<Signup />}/>
+        <Route path="/sign-in" element={<Sigin />}/>
+        <Route path="/about" element={<About />}/>
+         
+      </Routes>
+    </BrowserRouter>
   )
 }

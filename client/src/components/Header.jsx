c6f60e502ce243f4b142e712e528d0f68f041077
NarutoxParth <114332684+NarutoxParth@users.noreply.github.com>
2024-07-11 22:09:41 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Navbar } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <Navbar fluid rounded>
+    <Navbar fluid rounded className='border-b-2'>
         <Navbar.Brand as={Link} href="https://flowbite-react.com">
         <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
@@ -19,7 +20,7 @@ export default function Header() {
         <Navbar.Link href="#">Services</Navbar.Link>
         <Navbar.Link href="#">Pricing</Navbar.Link>
         <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>    
+      </Navbar.Collapse>
     </Navbar>
   )
 }
